bae6ec5830e10da530a66c3360ab25e817ccee2b
10Ngwako <ngoakoramakgoakgoa@gmail.com>
2025-11-17 14:23:59 +0200

    Added subscribe folder and file

---
+++ Pages/Subscribe/Subscribe.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesMovie.Pages.Subscribe
+{
+    public class SubscribeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
