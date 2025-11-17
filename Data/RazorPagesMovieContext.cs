f5af0f7f08c834a8382affdfb8029fe2bcfb8fa7
malopepetronella20 <malopepetronella20gmail.com>
2025-11-17 14:18:16 +0200

    updated RazorPagesMovie and added ApplicationDbContext.cs file

--- Data/RazorPagesMovieContext.cs
+++ Data/RazorPagesMovieContext.cs
@@ -2,18 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using RazorPagesMovie.Models;
 
-namespace RazorPagesMovie.Data
+public class RazorPagesMovieContext : IdentityDbContext
 {
-    public class RazorPagesMovieContext : DbContext
+    public RazorPagesMovieContext(DbContextOptions<RazorPagesMovieContext> options)
+        : base(options)
     {
-        public RazorPagesMovieContext(DbContextOptions<RazorPagesMovieContext> options)
-            : base(options)
-        {
-        }
-
-        public DbSet<RazorPagesMovie.Models.Movie> Movie { get; set; } = default!;
     }
+
+    // Your existing DbSets here
 }
+
+
