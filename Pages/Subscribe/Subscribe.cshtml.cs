815f0fc7dc8308390cdaa91c2f936ec14dafdb61
10Ngwako <ngoakoramakgoakgoa@gmail.com>
2025-11-18 09:23:22 +0200

    Revert "Added Subscribe.cshtml.cs"
    This reverts commit f67b8493507e7bbe1f6c4542587f3a943ece07d0.

--- Pages/Subscribe/Subscribe.cshtml.cs
+++ Pages/Subscribe/Subscribe.cshtml.cs
@@ -1,36 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-public class SubscribeModel : PageModel
+namespace RazorPagesMovie.Pages.Subscribe
 {
-    private readonly RazorPagesMovieContext _context;
-
-    public SubscribeModel(RazorPagesMovieContext context)
+    public class SubscribeModel : PageModel
     {
-        _context = context;
-    }
-
-    [BindProperty]
-    public Subscriber Subscriber { get; set; } = new Subscriber();
-
-    public async Task<IActionResult> OnPostAsync()
-    {
-        if (!ModelState.IsValid)
+        public void OnGet()
         {
-            return Page();
         }
-
-        // Check if email already exists
-        if (_context.Subscribers.Any(s => s.Email == Subscriber.Email))
-        {
-            ModelState.AddModelError(string.Empty, "This email is already subscribed.");
-            return Page();
-        }
-
-        _context.Subscribers.Add(Subscriber);
-        await _context.SaveChangesAsync();
-
-        TempData["Message"] = "Thank you for subscribing!";
-        return RedirectToPage("/Index");
     }
 }
